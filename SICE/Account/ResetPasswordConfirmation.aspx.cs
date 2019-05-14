46a6f8650d7397eec0e2d762b8dda1aec86f93de
Sebastian <grayjacketstudios@gmail.com>
2019-05-13 21:26:27 -0300

    Add project files.

---
+++ SICE/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SICE.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SICE/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SICE.Startup))]
+namespace SICE
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

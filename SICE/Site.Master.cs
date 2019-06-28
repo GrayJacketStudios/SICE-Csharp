e3edbae682f1d77990f5e092b8bf273da35b1da9
Eduardo Gamboa <efgm1993@gmail.com>
2019-06-27 22:26:30 -0300

    cambio de colores en el mapa de areas. Pendiente: desechar mapa, usar listado o timeline horizontal

--- SICE/Site.Master.cs
+++ SICE/Site.Master.cs
@@ -46,7 +46,7 @@ namespace SICE
             }
             if (!Request.IsAuthenticated && HttpContext.Current.Request.Url.AbsolutePath != "/Account/Login")
             {
-                HttpContext.Current.Response.Redirect("Account/Login");
+                //HttpContext.Current.Response.Redirect("Account/Login");
             }
 
 

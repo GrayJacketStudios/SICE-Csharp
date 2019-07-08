25dd70714feabce5cf435908e1db5503460ed04b
Sebastian <grayjacketstudios@gmail.com>
2019-07-08 00:03:35 -0300

    Se vuelve a reincorporar el inicio de sesion forzoso

--- SICE/Site.Master.cs
+++ SICE/Site.Master.cs
@@ -49,7 +49,7 @@ namespace SICE
             }
             if (!Request.IsAuthenticated && HttpContext.Current.Request.Url.AbsolutePath != "/Account/Login")
             {
-                //HttpContext.Current.Response.Redirect("Account/Login");
+                HttpContext.Current.Response.Redirect("Account/Login");
             }
 
 
